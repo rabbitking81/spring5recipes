0c378efd15b589ad4a021b399849d905229ab5be
danny <rabbitking81@gmail.com>
2020-12-13 02:44:01 +0900

    POJO 레퍼런스와 자동 연결을 이요해 다른 POJO 와 상호 작용하기 - @Qualifier로 모호한 자동 연결 명시하기

---
+++ ch02/recipe_2_3_6/src/main/java/com/rabbitking81/springrecipes/sequence/SequenceGenerator.java
@@ -0,0 +1,40 @@
+package com.rabbitking81.springrecipes.sequence;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class SequenceGenerator {
+    @Autowired
+    @Qualifier("datePrefixGenerator")
+    private PrefixGenerator prefixGenerator;
+    private String suffix;
+    private int initial;
+    private final AtomicInteger counter = new AtomicInteger();
+
+    public SequenceGenerator() {
+    }
+
+    public SequenceGenerator(PrefixGenerator prefixGenerator, String suffix, int initial) {
+        this.prefixGenerator = prefixGenerator;
+        this.suffix = suffix;
+        this.initial = initial;
+    }
+
+    public void setPrefixGenerator(PrefixGenerator prefixGenerator) {
+        this.prefixGenerator = prefixGenerator;
+    }
+
+    public void setSuffix(String suffix) {
+        this.suffix = suffix;
+    }
+
+    public void setInitial(int initial) {
+        this.initial = initial;
+    }
+
+    public synchronized String getSequence() {
+        return prefixGenerator.getPrefix() + initial + counter.getAndIncrement() + suffix;
+    }
+}
